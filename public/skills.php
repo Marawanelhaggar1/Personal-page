2fc8ed7be445b06fb357dec7ed711b959c2f657f
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-08-20 17:34:04 +0300

    final

--- public/skills.php
+++ public/skills.php
@@ -16,9 +16,21 @@ fclose($myfile);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .skill{
-            background-coloe:#eee
+      .skills .skill{
+            background-color:#eee;
             
+            width:90%;
+            margin:1rem 2rem;
+            height: 100px; 
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            font-size:20px;
+            
+        }
+
+        h1{
+            margin:1rem;
         }
     </style>
 </head>
@@ -36,7 +48,7 @@ fclose($myfile);
     <div class="skills">
         <?php 
                         foreach ($skills as $skill){
-                    echo "<div class='skill'><span>$skill->name</span>";
+                    echo "<div class='skill' ><span>$skill->name</span>";
 
                    echo "<span>$skill->level / 5</span>";
                     
